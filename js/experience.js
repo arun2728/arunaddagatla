3035135738b5c2b38694831a9e5644045242b32d
arun2728 <arun.a.addagatla@gmail.com>
2022-09-22 22:44:43 -0700

    Merged all pages

--- js/experience.js
+++ js/experience.js
@@ -29,11 +29,11 @@ $(document).ready(function() {
       });
     })
   });
-  
-// Get the button, and when the user clicks on it, execute myFunction
-document.getElementById("myBtn").onclick = function() {myFunction()};
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function myFunction1() {
+  document.getElementById("myexp1").classList.toggle("show");
+}
+function myFunction2() {
+  document.getElementById("myexp2").classList.toggle("show");
 }
