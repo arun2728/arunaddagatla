dd5c3871f0500b3cd950b4289bb7266517604bc5
arun2728 <arun.a.addagatla@gmail.com>
2022-09-22 22:25:07 -0700

    Merged all pages

---
+++ js/experience.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    var degrees = 0;
+    $('.img_new').click(function rotateMe(e) {
+      degrees += 360;
+      //$('.img').addClass('rotated'); // for one time rotation
+      $('.img_new').css({
+  
+        'transform': 'rotate(' + degrees + 'deg)',
+        '-ms-transform': 'rotate(' + degrees + 'deg)',
+        '-moz-transform': 'rotate(' + degrees + 'deg)',
+        '-webkit-transform': 'rotate(' + degrees + 'deg)',
+        '-o-transform': 'rotate(' + degrees + 'deg)'
+      });
+    })
+  });
+
+  $(document).ready(function() {
+    var degrees = 0;
+    $('.img_new1').click(function rotateMe(e) {
+      degrees += 360;
+      //$('.img').addClass('rotated'); // for one time rotation
+      $('.img_new1').css({
+  
+        'transform': 'rotate(' + degrees + 'deg)',
+        '-ms-transform': 'rotate(' + degrees + 'deg)',
+        '-moz-transform': 'rotate(' + degrees + 'deg)',
+        '-webkit-transform': 'rotate(' + degrees + 'deg)',
+        '-o-transform': 'rotate(' + degrees + 'deg)'
+      });
+    })
+  });
+  
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
